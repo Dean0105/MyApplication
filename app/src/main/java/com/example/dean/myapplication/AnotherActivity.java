c5edfca1db232ed39fc3e78d0b7752af2c6a9076
Dean <Dean0105@126.com>
2018-07-23 16:34:47 +0800

    1.测试

---
+++ app/src/main/java/com/example/dean/myapplication/AnotherActivity.java
@@ -0,0 +1,17 @@
+package com.example.dean.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Dean on 2018/7/23.
+ */
+
+public class AnotherActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}

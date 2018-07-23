d8a28a6bc81787946157f886fa5ff9abd1e3e350
Dean <Dean0105@126.com>
2018-07-23 16:41:33 +0800

    1.测试

--- app/src/main/java/com/example/dean/myapplication/AnotherActivity.java
+++ app/src/main/java/com/example/dean/myapplication/AnotherActivity.java
@@ -3,6 +3,7 @@ package com.example.dean.myapplication;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.Toast;
 
 /**
  * Created by Dean on 2018/7/23.
@@ -13,5 +14,7 @@ public class AnotherActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Toast.makeText(AnotherActivity.this,"测试",Toast.LENGTH_LONG).show();
     }
 }
